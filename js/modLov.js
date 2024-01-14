48a0594d3c03af527767f3933ba654a311520a2b
MedMax-76 <maximusmax76@gmail.com>
2024-01-14 22:58:49 +0200

    #

--- js/modLov.js
+++ js/modLov.js
@@ -7,3 +7,23 @@ setTimeout(function () {
       modalwin.style.display = "none";
     });
 }, 10000);
+
+setTimeout(function () {
+  const modalwin = document.getElementById("modnoye_okno");
+  modalwin.style.display = "block";
+  document
+    .getElementById("kaxalon_oyena")
+    .addEventListener("click", function () {
+      modalwin.style.display = "none";
+    });
+}, 20000);
+
+setTimeout(function () {
+  const modalwin = document.getElementById("modnoye_okno");
+  modalwin.style.display = "block";
+  document
+    .getElementById("kaxalon_oyena")
+    .addEventListener("click", function () {
+      modalwin.style.display = "none";
+    });
+}, 30000);
